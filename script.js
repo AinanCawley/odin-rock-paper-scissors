c786e211d5b9abc07ae8ca68976480e69c279712
AinanCawley <ainan.c.cawley@gmail.com>
2022-04-21 12:06:50 +0800

    Make round evaluation function

--- script.js
+++ script.js
@@ -50,5 +50,52 @@ function computerPlay()
 
 function evaluateRound(playerSelection, computerSelection)
 {
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+
+    let playerStatus;
+
+    if(playerSelection=="ROCK")
+    {
+        if(computerSelection=="PAPER")
+        {
+            playerStatus = 0;
+        }
+        if(computerSelection=="SCISSORS")
+        {
+            playerStatus = 1;
+        }
+    }
+    if(playerSelection=="PAPER")
+    {
+        if(computerSelection=="ROCK")
+        {
+            playerStatus = 1;
+        }
+        if(computerSelection=="SCISSORS")
+        {
+            playerStatus = 0;
+        }
+    }
+    if(playerSelection=="SCISSORS")
+    {
+        if(computerSelection=="ROCK")
+        {
+            playerStatus = 0;
+        }
+        if(computerSelection=="PAPER")
+        {
+            playerStatus = 1;
+        }
+    }
+
+    if(playerStatus==0)
+    {
+        return "You lose!";
+    }
+    if(playerStatus==1)
+    {
+        return "You win!";
+    }
 
 }
